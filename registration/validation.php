ff6af67cc5fdf74ff2b8020623323a5e1100c6f4
jakemurphy217 <jakemurphy217@gmail.com>
2018-12-13 14:48:37 +0000

    Add files via upload

--- registration/validation.php
+++ registration/validation.php
@@ -1,29 +1,28 @@
 <?php
 session_start();
-header('location:Login.php');
 $con = mysqli_connect('localhost','root','','registration');
 if(!$con){
   die("db error");
 }
 if(isset($_POST["submit"])){
+ 
   $username = $_POST["name"];
   $password = $_POST["password"];
+  
   if(!empty($username) && !empty($password)){
     $username = mysqli_real_escape_string($con, $username); 
     $password = mysqli_real_escape_string($con, $password);
     
-    $query = "SELECT * FROM usertable WHERE name = '$username'";
+    $query = "SELECT * FROM usertable WHERE name = '$username' && password = '$password'";
     $result = mysqli_query($con, $query);
-    
+  
     if(mysqli_num_rows($result) == 1){
-      echo "Username already taken";
-    } else {
-      $query = "INSERT INTO usertable (name, password) VALUES('$username', '$password')";
-      $result = mysqli_query($con, $query);
-      echo "Registration Successful";
+      header('location:index.php');
+    }else{
+       header('location:Login.php');
     }
   }
-} else {
+}else {
   echo "Something went wrong";
 }
       
